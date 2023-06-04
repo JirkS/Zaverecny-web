2f8be928d9721fba21cde202180b715a43fdc83c
Jiří Syrovátko <113086098+JirkS@users.noreply.github.com>
2023-06-04 14:33:34 +0200

    Add files via upload

--- login/register.php
+++ login/register.php
@@ -16,13 +16,29 @@ if(insertUser($_POST["first_name"], $_POST["last_name"], $_POST["age"], $_POST["
 
 function insertUser($first_name, $last_name, $age, $country, $email, $password, $password2): bool
 {
-    if($password == $password2 && $age > 0 && $age < 120){
-        $pass = password_hash($password, PASSWORD_DEFAULT);
-        $connection = DBC::getConnection();
-        $statement = $connection->prepare("INSERT INTO Users (first_name, last_name, age, country, email, pass) VALUES (?, ?, ?, ?, ?, ?)");
-        $statement->bind_param("ssisss", $first_name, $last_name, $age, $country, $email, $pass);
-        return $statement->execute();
+    if(checkEmail($email)) {
+        if($password === $password2 && $age > 0 && $age < 120){
+            $pass = password_hash($password, PASSWORD_DEFAULT);
+            $connection = DBC::getConnection();
+            $statement = $connection->prepare("INSERT INTO Users (first_name, last_name, age, country, email, pass) VALUES (?, ?, ?, ?, ?, ?)");
+            $statement->bind_param("ssisss", $first_name, $last_name, $age, $country, $email, $pass);
+            return $statement->execute();
+            DBC::closeConnection();
+        }
+        return false;
     }
     return false;
+}
+
+function checkEmail($email): bool {
+    $connection = DBC::getConnection();
+    $statement = $connection->prepare("SELECT * FROM users where email = '$email'");
+    $statement->execute();
+    $result = $statement->get_result();
+    $usersEmail = $result->fetch_all()[0];
+    if($usersEmail > 0){
+        return false;
+    }
+    return true;
 }
 ?>
